c63f8bb80d5313d81e195d19036c179607d818bc
Adekunle Ismail <adismail4@gmail.com>
2022-08-10 21:23:33 +0100

    Redirect to categories path after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
- # before_action :authenticate_user!
-
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,4 +7,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def after_sign_in_path_for(resources)
+    categories_path 
+  end
+
 end
