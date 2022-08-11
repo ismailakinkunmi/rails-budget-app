b0b7175dd082828088c8e860d7eab440fc260a17
Adekunle Ismail <adismail4@gmail.com>
2022-08-11 18:01:02 +0100

    Modify category and transaction resouces

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   
   devise_for :users
 
-  resources :categories do
-    resources :transactions
+  resources :categories, only:[:index, :new, :create] do
+    resources :transactions, only:[:index, :new, :create]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
